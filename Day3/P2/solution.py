a8b100723bb8ef6b1ece09527c1b1f48b4cd8b05
Ritik <ritikaryan0072002@gmail.com>
2025-12-26 16:57:05 +0530

    Modified solution of Problem 2, Day 3

--- Day3/P2/solution.py
+++ Day3/P2/solution.py
@@ -1,30 +1,31 @@
-from tqdm import tqdm
-
 with open("input.txt") as file:
     data = [f.strip('\n') for f in file.readlines()]
 # print(data)
 
-ans = 0
+ans = []
 
-for s in tqdm(data):
-    max_num = -1
-    num = 0
-    for i in range(len(s)):
-        for i2 in range(i + 1, len(s)):
-            for i3 in range(i2+1, len(s)):
-                for i4 in range(i3+1, len(s)):
-                    for i5 in range(i4+1, len(s)):
-                        for i6 in range(i5+1, len(s)):
-                            for i7 in range(i6+1, len(s)):
-                                for i8 in range(i7+1, len(s)):
-                                    for i9 in range(i8+1, len(s)):
-                                        for i10 in range(i9+1, len(s)):
-                                            for i11 in range(i10+1, len(s)):
-                                                for i12 in range(i11+1, len(s)):
-                                                    num = int(s[i]+s[i2]+s[i3]+s[i4]+s[i5]+s[i6]+s[i7]+s[i8]+s[i9]+s[i10]+s[i11]+s[i12])
-                                                    if num > max_num:
-                                                        max_num = num
-    # print(max_num)
-    ans += int(max_num)
 
-print(ans)
+for s in data:
+    stk = []
+    n = len(s)
+    k = n - 12
+    for ch in s:
+        # if (len(stk) == 0 or k == 0):
+        #     stk.append(ch)
+        # elif (ch > stk[-1]):
+        #     stk.pop()
+        #     stk.append(ch)
+        #     k -= 1
+        # elif (len(stk) <12):
+        #     stk.append(ch)
+        while k > 0 and stk and stk[-1] < ch:
+            stk.pop()
+            k -= 1
+        stk.append(ch)
+    # print(stk)
+    if (k > 0):
+        stk = stk[:-k]
+    stk = stk[:12]
+    ans.append(int(''.join(stk)))
+
+print(sum(ans))
